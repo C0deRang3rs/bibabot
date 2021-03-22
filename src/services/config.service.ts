dfc84ceaff1e58aeadffd67ad3da8b23b5d7b2c1
Vladyslav Berezhnyi <bakuganxd@gmail.com>
2021-03-22 22:49:18 +0200

    Bugfix

--- src/services/config.service.ts
+++ src/services/config.service.ts
@@ -2,18 +2,15 @@ import { Context } from 'telegraf/typings/context';
 import { Markup } from 'telegraf';
 import { InlineKeyboardMarkup, Message } from 'telegraf/typings/core/types/typegram';
 import BaseService from './base.service';
-import { BotCommandType, BotListener, CommandType } from '../types/core/bot.types';
-import { BotAction, CommandCategory, ConfigCommand } from '../types/globals/commands.types';
+import { BotCommandType, BotListener } from '../types/core/bot.types';
+import { BotAction, ConfigCommand } from '../types/globals/commands.types';
 import {
-  ConfigAction, ConfigProperty, getPropertyDescription, IndividualConfigProperty,
+  ConfigAction, ConfigProperty, getPropertyDescription,
 } from '../types/services/config.service.types';
 import ConfigRepository from '../repositories/config.repo';
 import DeleteLastMessage from '../decorators/delete.last.message.decorator';
 import DeleteRequestMessage from '../decorators/delete.request.message.decorator';
 import { BotMessage } from '../types/globals/message.types';
-import ReplyWithError from '../decorators/reply.with.error.decorator';
-import CommandTemplate from '../decorators/command.template.decorator';
-import RepliableError from '../types/globals/repliable.error';
 
 export default class ConfigService extends BaseService {
   private static instance: ConfigService;
@@ -32,48 +29,6 @@ export default class ConfigService extends BaseService {
     }
 
     return ConfigService.instance;
-  }
-
-  @DeleteRequestMessage()
-  @ReplyWithError()
-  @CommandTemplate([CommandType.COMMAND, CommandType.NUMBER])
-  private async changeJailMinVote(ctx: Context): Promise<Message> {
-    if (
-      !ctx.from
-      || !ctx.chat
-      || !ctx.message
-      || !('text' in ctx.message)
-    ) {
-      throw new Error('Wrong context');
-    }
-
-    // TODO: вынести команды для создателя чата / админов с высокими правами
-    const chatId = ctx.chat.id;
-    const admins = await this.bot.app.telegram.getChatAdministrators(ctx.chat.id);
-    const isUserCreator = admins.find((user) => user.user.id === ctx.from!.id)?.status === 'creator';
-
-    if (!isUserCreator) {
-      throw new RepliableError('Команда доступна только основателю чата', ctx);
-    }
-
-    const membersCount = await this.bot.app.telegram.getChatMembersCount(chatId);
-    const minVoteCount = parseInt(ctx.message.text.split(' ')[1], 10);
-
-    if (minVoteCount > membersCount) {
-      throw new RepliableError('Нельзя указать больше чем кол-во пользователей в чате', ctx);
-    }
-
-    if (minVoteCount < 1) {
-      throw new RepliableError('Нельзя указать меньше чем 1', ctx);
-    }
-
-    await this.configRepo.setConfigIndividualProperty(
-      chatId,
-      IndividualConfigProperty.JAIL_MIN_VOTE_COUNT,
-      minVoteCount.toString(),
-    );
-
-    return ctx.reply(`Минимальное количество голосов для бана изменено на ${minVoteCount}`);
   }
 
   @DeleteRequestMessage()
@@ -97,13 +52,6 @@ export default class ConfigService extends BaseService {
         name: ConfigCommand.CONFIG,
         description: 'Настройки бота для данного чата',
         callback: (ctx): Promise<Message> => this.configMenu(ctx),
-      },
-      {
-        type: BotCommandType.COMMAND,
-        name: ConfigCommand.MIN_VOTE_COUNT,
-        category: CommandCategory.JAIL,
-        description: 'Изменить колво голосов для бана. (Доступно только для основателя чата)',
-        callback: (ctx): Promise<Message> => this.changeJailMinVote(ctx),
       },
     ] as BotListener[];
 
